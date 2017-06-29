3b51e45a9078f5b9bba04002751314b6a5da2b83
Colin Gross <grosscol@gmail.com>
2017-06-29 15:47:13 -0400

    Fill out the basic magnitude tests

--- spec/say_spec.rb
+++ spec/say_spec.rb
@@ -22,33 +22,36 @@ RSpec.describe Say do
       expect(Say.new(20).in_english ).to eq("twenty")
     end
 
+    it "works with teens" do
+      expect(Say.new(13).in_english ).to eq("thirteen")
+      expect(Say.new(15).in_english ).to eq("fifteen")
+    end
 
     it "works with hundreds" do
-
+      expect(Say.new(100).in_english ).to eq("one hundred")
+      expect(Say.new(200).in_english ).to eq("two hundred")
+      expect(Say.new(300).in_english ).to eq("three hundred")
+      expect(Say.new(400).in_english ).to eq("four hundred")
     end
 
     it "works with thousands" do
-
+      expect(Say.new(4000).in_english ).to eq("four thousand")
+      expect(Say.new(5000).in_english ).to eq("five thousand")
+      expect(Say.new(6000).in_english ).to eq("six thousand")
+      expect(Say.new(7000).in_english ).to eq("seven thousand")
     end
 
     it "works with millions" do
-      
+      expect(Say.new(7_000_000).in_english ).to eq("seven million")
     end
 
     it "works with billions" do
-
+      expect(Say.new(8_000_000_000).in_english ).to eq("eight billion")
     end
 
     it "works with trillions" do
-
-    end
-
-    it "nests tens in hundreds" do
-
-    end
-
-    it "nests hundreds in thousands" do
-
+      skip "Remains to be implemented"
+      expect(Say.new(9_000_000_000_000).in_english ).to eq("nine trillion")
     end
 
   end
